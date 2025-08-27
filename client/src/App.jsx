1b2dd2f9b344f5cadc3a397a77de6e5ba8181f7e
rampradops28 <rampradop28@gmail.com>
2025-08-27 19:56:30 +0530

    error rectified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/Toaster";
+import { TooltipProvider } from "@/components/ui/tooltip"; 
 import { useAuth } from "@/hooks/UseAuth";
 import LoginPage from "./pages/Login";
 import DashboardPage from "./pages/DashboardPage";
